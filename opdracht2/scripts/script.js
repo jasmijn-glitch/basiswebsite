f2028c50175be6bb893cab311c15ccf51a8d39b4
jasmijn-glitch <82438075+jasmijn-glitch@users.noreply.github.com>
2021-06-02 14:46:33 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("howdy");
+
+var gridButton = document.querySelector("#view-grid");
+var listButton = document.querySelector("#view-list");
+
+gridButton.addEventListener("change", toGrid);
+
+listButton.addEventListener("change", toList);
+
+function toGrid() {
+    document.body.classList.add("toonAlsGrid");
+    document.body.classList.remove("toonAlsList");
+}
+
+function toList() {
+    document.body.classList.add("toonAlsList");
+    document.body.classList.remove("toonAlsGrid");
+}
+
+
